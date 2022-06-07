3e6d63c85bf0374a0e9fc34a4a5b61c3236444af
鱼塘 <yutang@dian.so>
2022-06-07 19:42:45 +0800

    feat: 支持qiankun

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,38 @@
 // app entry
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { renderWithQiankun, qiankunWindow } from 'vite-plugin-qiankun/dist/helper'
 
-ReactDOM.createRoot(document.getElementById('app')).render(
-  <React.StrictMode>
-    <div>App</div>
-  </React.StrictMode>
-)
-  
+type AppProp = {
+  [key: string]: any
+}
+
+let root
+
+function render (props: AppProp) {
+  root = ReactDOM.createRoot(document.getElementById('app'))
+  root.render(
+    <React.StrictMode>
+      <div>App</div>
+    </React.StrictMode>
+  )
+}
+
+if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
+  render({})
+}
+
+renderWithQiankun({
+  mount(props) {
+    console.log('mount')
+    render(props)
+  },
+  bootstrap() {
+    console.log('bootstrap')
+  },
+  unmount() {
+    root.unmount()
+  },
+  update() {
+  }
+})
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import qiankun from 'vite-plugin-qiankun'
 import pkg from './package.json'
 
 const cwd = process.cwd()
@@ -14,13 +15,27 @@ const server = {
   host: '0.0.0.0',
 }
 
+let base = '/'
+if (process.env.NODE_ENV === 'production') {
+  // 这里改成你的项目的线上地址，推荐通过环境变量注入
+  base = process.env.ENV_PUBLIC_PATH
+} else {
+  base = `http://localhost:${server.port}/`
+}
+
+const reactPlugins = react().filter(item => item.name !== 'vite:react-refresh')
+
 export default defineConfig({
+  base,
   resolve: {
     alias: [
       { find: '@', replacement: path.join(cwd, './src') },
     ],
   },
-  plugins: [react()],
+  plugins: [
+    reactPlugins,
+    qiankun(pkg.name, { useDevMode: true }),
+  ],
   optimizeDeps: {
   },
   server,
