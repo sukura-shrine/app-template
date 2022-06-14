90daa1070ed1cbaa24e9f2a17896149ecc9b2acf
hakushinring <hakushinring@gmail.com>
2022-06-14 10:52:33 +0800

    chore: shrine config add plugins

--- shrine.config.js
+++ shrine.config.js
@@ -1,5 +1,11 @@
+import devServer from '@hakushin/plugin-dev-server'
+import devConsole from '@hakushin/plugin-dev-console'
+
 export default {
+  type: 'pc',
   port: 3999,
   plugins: [
+    devServer(),
+    devConsole(),
   ],
 }
