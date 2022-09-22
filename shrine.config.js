6174b364cfbf715c4cb5220cbb0df50d39dc19e6
hakushinring <hakushinring@gmail.com>
2022-09-23 02:26:20 +0800

    chore: update config

--- shrine.config.js
+++ shrine.config.js
@@ -1,11 +1,9 @@
-// import devServer from '@hakushin/plugin-dev-server'
 // import devConsole from '@hakushin/plugin-dev-console'
 
 export default {
   type: 'pc',
   port: 3200,
   plugins: [
-    // devServer(),
     // devConsole(),
   ],
 }
